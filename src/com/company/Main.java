69c14171b768eadd7c4cf5d4978e32edfdb64921
Barry <bbalasingham@gmail.com>
2021-06-17 23:20:06 -0700

    [Big Brain Update] Can now choose what gets spammed into chat and spamming is smarter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,54 @@
 package com.company;
 
 import java.awt.*;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.awt.event.KeyEvent;
+import java.util.Scanner.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         Robot robot = new Robot();
-        int keyCode = 67;
+
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString = scanner.nextLine();
 
         TimeUnit.SECONDS.sleep(3);
 
-        for (int i = 0; i < 100000; i++) {
-            for (int j = 0; j < 10; j++) {
+        int[] printValues = convertStringToASCII(inputString);
 
-                robot.keyPress(keyCode);
-                robot.keyRelease(keyCode);
-
-                TimeUnit.MILLISECONDS.sleep(10);
-
-                robot.keyPress(10);
-                robot.keyRelease(10);
+        for (int i = 0; i < 1000000; i++) {
+            for (int j = 0; j < printValues.length; j++) {
+                robot.keyPress(printValues[j]);
+                robot.keyRelease(printValues[j]);
             }
-            TimeUnit.SECONDS.sleep(9);
+            TimeUnit.MILLISECONDS.sleep(5);
+
+            robot.keyPress(10);
+            robot.keyRelease(10);
+
+            TimeUnit.SECONDS.sleep(1);
         }
+    }
+
+    public static int[] convertStringToASCII(String string) {
+
+        char[] stringChars = string.toCharArray();
+        int[] returnValues = new int[string.length()];
+
+        for (int i = 0; i < string.length(); i++) {
+            returnValues[i] = stringChars[i] - 32;
+            if (Character.isUpperCase(stringChars[i])) {
+                returnValues[i] = returnValues[i] + 64;
+            }
+
+            if (stringChars[i] == ' ') {
+                returnValues[i] = 32;
+            }
+        }
+
+        return returnValues;
     }
 }
